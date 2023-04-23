c4958f97a74d00e92f5e03e39ed2eb880f6fc01b
CoderValencia <valencia.coleman625@gmail.com>
2023-04-23 00:19:00 -0400

    Updated Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,33 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public CharacterController controller;
-    public Transform cam;
+    public float moveSpeed;
+    public Transform orientation;
 
-    public float speed = 6f;
+    public float groundDrag;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public bool readyToJump;
+
+    public float playerHeight;
+    public  LayerMask ground;
+    bool grounded;
+
+   
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+     Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        readyToJump = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction  = new Vector3(horizontal,0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+        PlayerInput();
+        SpeedControl();
+
+        if (grounded) 
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+       
+    }
+    
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            Jump();
+           
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+        else if (readyToJump && grounded) {
+
+            rb.constraints = RigidbodyConstraints.FreezeRotation ;
+        }
+       
+    }
+
+    private void Move()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+
+        //on ground
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+       
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitVel.x, rb.velocity.y, limitVel.z);
         }
     }
+
+    private void Jump()
+    {
+        
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+
+        readyToJump= true;
+        if (grounded)
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
+        }
+       
+
+    }
+
 }
