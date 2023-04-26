8616b49fd090034f4fafebcbae176515a09e1584
CoderValencia <valencia.coleman625@gmail.com>
2023-04-26 13:44:04 -0400

    Added Pushing

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
@@ -9,5 +10,67 @@ public class PickUp : MonoBehaviour
     public Transform player, itemContainer;
 
     public float pickUpRange;
-    
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+
+    private void Start()
+    {
+        if(!equipped)
+        {
+            rb.isKinematic= false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+
+        }
+    }
+    private void PickUpFunction()
+    {
+        equipped = true;
+
+        transform.SetParent(itemContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one; 
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+    }
+
+    private void DropFunction()
+    {
+
+        equipped = true;
+
+        transform.SetParent(null);
+
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;    
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player is inside  pick up space");
+          
+
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player left pick up space");
+           
+
+        }
+
+    }
 }
