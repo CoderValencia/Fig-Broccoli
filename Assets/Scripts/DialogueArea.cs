a0f1b3cdb9c83550addfcc5807c3fe2c9d9068db
Celtican <Celtican5@gmail.com>
2023-04-27 13:22:31 -0400

    Removed unnecessary namespaces; Updated .gitignore

--- Assets/Scripts/DialogueArea.cs
+++ Assets/Scripts/DialogueArea.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting.FullSerializer;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
