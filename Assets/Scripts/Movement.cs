6e3c63eddaf72e4dba7ef92c33517884e2020b29
CoderValencia <valencia.coleman625@gmail.com>
2023-05-04 21:26:22 -0400

    Updated Movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,54 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
     public float turnSpeed;
-    public Transform orientation;
-
     public float groundDrag;
 
     public float jumpForce;
     public float jumpCooldown;
-    public float airMultiplier;
     public bool readyToJump;
 
     public float playerHeight;
     public  LayerMask ground;
     bool grounded;
 
-   
-
     float horizontalInput;
     float verticalInput;
 
-    Vector3 moveDirection;
+
 
     Rigidbody rb;
-    Animator anim;
-
-
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-
-
+       
         readyToJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
         PlayerInput();
-        SpeedControl();
         Move();
-
-
+       
 
         if (grounded) 
         {
@@ -61,12 +50,6 @@ public class Movement : MonoBehaviour
 
     }
     
-
-    void FixedUpdate()
-    {
-       
-    }
-
     private void PlayerInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -74,107 +57,37 @@ public class Movement : MonoBehaviour
 
         if(Input.GetKey(KeyCode.Space) && readyToJump && grounded)
         {
-            readyToJump = false;
-
-          // rb.constraints = RigidbodyConstraints.FreezeRotation;
+            readyToJump = false; 
             Jump();
-           
-
             Invoke(nameof(ResetJump), jumpCooldown);
-        }
-        else if (readyToJump && grounded) {
-
-            //rb.constraints = RigidbodyConstraints.FreezeRotation ;
-        }
-       
+        }       
     }
     
     private void Move()
     {
-
-  
-
-
         //on ground
         if (grounded)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
-
-            transform.Translate(Vector3.right* turnSpeed * horizontalInput * Time.deltaTime);
-
-            Debug.Log(verticalInput);
-            Debug.Log(horizontalInput);
-
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+  
         }
         else if (!grounded)
         {
-
-            if (Input.GetKey(KeyCode.S))
-            {
-                anim.Play("MoveDown");
-                moveDirection = -orientation.forward * verticalInput + orientation.right * horizontalInput;
-                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
-            }
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                anim.Play("MoveUp");
-                moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
-            }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                anim.Play("MoveLeft");
-                moveDirection = new Vector3(-1, 0, 0);
-
-                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
-
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                anim.Play("MoveRight");
-                moveDirection = new Vector3(1, 0, 0);
-
-                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
-
-            }
-
-            //rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
+            rb.freezeRotation= true;
         }
-
-    }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if(flatVel.magnitude > moveSpeed)
-        {
-            Vector3 limitVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitVel.x, rb.velocity.y, limitVel.z);
-        }
+    
     }
 
     private void Jump()
     {
-        
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     private void ResetJump()
     {
-
         readyToJump= true;
-        if (grounded)
-        {
-            //rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
-        }
-       
-
     }
-
 }
