80719fd11bc66ae22348f46af415ac41fce42274
lumi <111014364+lumienaut@users.noreply.github.com>
2023-05-24 21:28:46 -0700

    Final Level 1 + 2 Design

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class ObjectInteraction : MonoBehaviour
 {
     private bool interactActive = false;
-    public GameObject interactable;
-    public GameObject canvasE;
-    public GameObject canvasInstructions;
+    private bool hasInteracted = false;
+    public bool nothingHere = false;
+
+    // Quest object to reveal
+    public GameObject questObject;
+
+    // Canvas that says "Press E to Interact"
+    public GameObject canvasE; 
+
+    // Canvas that shows result of interaction
+    public GameObject canvasResult;
+
+    // Canvas that tells player there's nothing else here
+    public GameObject canvasHasInteracted;
+
+
     // Start is called before the first frame update
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && hasInteracted == false)
         {
             interactActive = true;
             canvasE.SetActive(true);
+            canvasHasInteracted.SetActive(false);
+        }
+
+        if (other.CompareTag("Player") && hasInteracted == true)
+        {
+            interactActive = true;
+            nothingHere = true;
         }
     }
 
@@ -25,18 +45,26 @@ public class ObjectInteraction : MonoBehaviour
         {
             interactActive = false;
             canvasE.SetActive(false);
-            canvasInstructions.SetActive(false);
+            canvasResult.SetActive(false);
+            canvasHasInteracted.SetActive(false);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (interactActive && Input.GetKeyDown(KeyCode.E))
+        if (interactActive && nothingHere == false && Input.GetKeyDown(KeyCode.E))
         {
-            interactable.SetActive(true);
+            questObject.SetActive(true);
             canvasE.SetActive(false);
-            canvasInstructions.SetActive(true);
+            canvasResult.SetActive(true);
+            hasInteracted = true;
         }
+
+        if (interactActive && nothingHere == true && Input.GetKeyDown(KeyCode.E))
+        {
+            canvasHasInteracted.SetActive(true);
+        }
+
     }
 }
